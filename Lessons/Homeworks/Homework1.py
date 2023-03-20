7c874f967b6f447b2b0da2bd9dee8b16276e80e0
aspramparsadanyan <aspram.parsadanyan@gmail.com>
2023-03-20 23:50:22 +0400

    Homework2

--- Lessons/Homeworks/Homework1.py
+++ Lessons/Homeworks/Homework1.py
@@ -1,92 +1,109 @@
-'''1. For this challenge, you are supposed to find the stum of the digits of a two-digit number.
-45 ➞ 9 38 ➞ 11 67 ➞ 13 '''
+# '''1. For this challenge, you are supposed to find the stum of the digits of a two-digit number.
+# 45 ➞ 9 38 ➞ 11 67 ➞ 13 '''
 
-input = 45
-sum = (input % 10)+ (input // 10) == 9
-print(sum)
+# input = 45
+# sum = (input % 10)+ (input // 10) == 9
+# print(sum)
 
 
-input = 38
-sum = (input % 10) + (input // 10) == 11
-print(sum)
+# input = 38
+# sum = (input % 10) + (input // 10) == 11
+# print(sum)
 
-input = 67
-sum = (input % 10) + (input // 10) == 13
-print(sum)
+# input = 67
+# sum = (input % 10) + (input // 10) == 13
+# print(sum)
 
-'''Need extra knowledge!! 
-2. A repdigit is a positive number composed out of the same digit. Create a function that takes an two-digit integer and returns whether it's a repdigit or not.
-44 ➞ True
-45 ➞ False
--44 ➞ False'''
+# '''Need extra knowledge!! 
+# 2. A repdigit is a positive number composed out of the same digit. Create a function that takes an two-digit integer and returns whether it's a repdigit or not.
+# 44 ➞ True
+# 45 ➞ False
+# -44 ➞ False'''
 
-def is_number_repdigit(num):
-     return (num % 10) == (num // 10)
-print(is_number_repdigit(44))
-print(is_number_repdigit(45))
-print(is_number_repdigit(-44))
+# def is_number_repdigit(num):
+#      return (num % 10) == (num // 10)
+# print(is_number_repdigit(44))
+# print(is_number_repdigit(45))
+# print(is_number_repdigit(-44))
 
  
-'''3. Write a function that takes an integer minutes and converts it to seconds.
-5 ➞ 300
-2 ➞ 120'''
+# '''3. Write a function that takes an integer minutes and converts it to seconds.
+# 5 ➞ 300
+# 2 ➞ 120'''
 
-def convert_minute_to_second(min):
-    return(min * 60)
-print( convert_minute_to_second(5) )
-print( convert_minute_to_second(2))
-
-
-
-'''4. Create a function that takes the age in years and returns the age in days.
-65 ➞ 23725
-0 ➞ 0
-20 ➞ 7300'''
-
-def year_in_days(year):
-    return(year * 365)
-print(year_in_days(65))
-print(year_in_days(0))
-print(year_in_days(20))
+# def convert_minute_to_second(min):
+#     return(min * 60)
+# print( convert_minute_to_second(5) )
+# print( convert_minute_to_second(2))
 
 
 
-'''5. Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
-1,3 ➞ 3780
-2,0 ➞ 7200'''
+# '''4. Create a function that takes the age in years and returns the age in days.
+# 65 ➞ 23725
+# 0 ➞ 0
+# 20 ➞ 7300'''
 
-def hour_in_second(hour,minute):
-    return(hour * 60 * 60) + (minute * 60)
-print(hour_in_second(1, 3))
-print(hour_in_second(2,0))
-
-
-
-
-'''6. Create a function that accepts a measurement value in inches 
-and returns the equivalent of the measurement value in feet.'''
-def inch_to_feet(inch, one_inch_in_feet):
-    return(inch * one_inch_in_feet) 
-print(inch_to_feet(100, 0.0833333333))
+# def year_in_days(year):
+#     return(year * 365)
+# print(year_in_days(65))
+# print(year_in_days(0))
+# print(year_in_days(20))
 
 
-'''7. Create a function that takes a number 
-as an argument and returns "even" for even numbers and "odd" for odd numbers.'''
 
-def even_or_odd(num):
-    if num % 2 == 0:
-        return "even"
-    else:
-        return "odd"
-print(even_or_odd(5))
-print(even_or_odd(8))  
-print(even_or_odd(1)) 
+# '''5. Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+# 1,3 ➞ 3780
+# 2,0 ➞ 7200'''
 
-def even_or_odd(num):
-    return ("odd", "even")[num % 2 == 0]
-result = even_or_odd(11)
-print(result)  
+# def hour_in_second(hour,minute):
+#     return(hour * 60 * 60) + (minute * 60)
+# print(hour_in_second(1, 3))
+# print(hour_in_second(2,0))
 
+
+
+
+# '''6. Create a function that accepts a measurement value in inches 
+# and returns the equivalent of the measurement value in feet.'''
+# def inch_to_feet(inch, one_inch_in_feet):
+#     return(inch * one_inch_in_feet) 
+# print(inch_to_feet(100, 0.0833333333))
+
+
+# '''7. Create a function that takes a number 
+# as an argument and returns "even" for even numbers and "odd" for odd numbers.'''
+
+# def even_or_odd(num):
+#     if num % 2 == 0:
+#         return "even"
+#     else:
+#         return "odd"
+# print(even_or_odd(5))
+# print(even_or_odd(8))  
+# print(even_or_odd(1)) 
+
+# def even_or_odd(num):
+#     return ("odd", "even")[num % 2 == 0]
+# result = even_or_odd(11)
+# print(result)  
+
+
+x= 7
+y= x % 2
+print_text= "Zuyg" * (y == 0) + "kent" * y
+print(print_text)
+
+
+# birth_year= 1999
+# this_year = 2023
+# age = (this_year - birth_year)
+# days = (age//4) + (age*365)
+# print(days)
+
+# birth_year = 1995
+# age = (2023- birth_year) 
+# z=(age >= 20 and age <= 30) * "vayelir" + (age> 30 and age <= 40) * "ushe"
+# print(z)
 
 
 
